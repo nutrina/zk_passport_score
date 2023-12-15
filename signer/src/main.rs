486c05104ac16ea7dcb705fccb67054f4334320a
schultztimothy <schultz.timothy52@gmail.com>
2023-12-14 20:29:02 -0700

    feat: rust big signature

--- signer/src/main.rs
+++ signer/src/main.rs
@@ -1,31 +1,37 @@
 use anyhow::Result;
 use ecdsa::{
     signature::{self, rand_core::OsRng, Signer, Verifier},
-    Signature, SigningKey, VerifyingKey,
+    EncodedPoint, Signature, SigningKey, VerifyingKey,
 };
-use k256::ecdsa::{signature::Signer as DigestSigner, Signature as EcdsaSignature};
-use sha2::Sha256;
+use k256::{
+    ecdsa::{signature::Signer as DigestSigner, Signature as EcdsaSignature},
+    Secp256k1,
+};
+use sha2::{Digest, Sha256};
 
-fn main() -> Result<()> {
-    // Generate a new ECDSA key pair
-    let signing_key = SigningKey::random(&mut OsRng);
+fn hash_and_sign_message(
+    message: &[u8],
+    signing_key: &SigningKey<Secp256k1>,
+    verify_key: &VerifyingKey<Secp256k1>,
+) {
+    // Hash the data using SHA-256 to get a 32-byte hash
+    let mut hasher = Sha256::new();
+    hasher.update(message);
+    let message_hash = hasher.finalize();
 
-    // Obtain the verifying key (public key)
-    let verify_key = VerifyingKey::from(&signing_key);
-
-    // Data to be signed
-    let data = b"Example data to sign";
+    // Ensure the hash is 32 bytes
+    assert_eq!(message_hash.len(), 32, "Hashed data is not 32 bytes");
 
     // Sign the data directly (without manually hashing it)
     // Explicitly specifying the type of the signature
-    let signed_msg: EcdsaSignature = signing_key.sign(data);
+    let signed_msg: EcdsaSignature = signing_key.sign(&message_hash);
 
     // Serialize the signature into a byte array
     let signature_bytes = signed_msg.to_vec();
     println!("Signature: {:?}", signature_bytes);
 
     // Verify the signature
-    let is_valid = verify_key.verify(data, &signed_msg);
+    let is_valid = verify_key.verify(message, &signed_msg);
 
     match is_valid {
         Ok(_) => println!("Signature is valid!"),
@@ -33,6 +39,35 @@ fn main() -> Result<()> {
             dbg!(e);
         }
     }
+
+    // Convert the verifying key to an EncodedPoint
+    let encoded_point: EncodedPoint<Secp256k1> = verify_key.to_encoded_point(false); // false for uncompressed
+
+    // Convert to uncompressed format as a byte array
+    let uncompressed_bytes = encoded_point.as_bytes();
+
+    let x_key_coordinates = &uncompressed_bytes[1..33];
+    let y_key_coordinates = &uncompressed_bytes[33..65];
+
+    dbg!(
+        x_key_coordinates,
+        y_key_coordinates,
+        signature_bytes,
+        message_hash
+    );
+}
+
+fn main() -> Result<()> {
+    // Generate a new ECDSA key pair
+    let signing_key = SigningKey::random(&mut OsRng);
+
+    // Obtain the verifying key (public key)
+    let verify_key: VerifyingKey<Secp256k1> = VerifyingKey::from(&signing_key);
+
+    // Data to be signed
+    let message = b"GqmK8ClmCF6E9DaQYe3ei3KGlwyJOWDPNthLX4NRftQ=googlegooglegooglegooglegoogle";
+
+    hash_and_sign_message(message, &signing_key, &verify_key);
 
     Ok(())
 }
